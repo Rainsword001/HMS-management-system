020ee9766be4d21e43a9bed97f053f3db2073221
Rainsword001 <olowoyoemmanuel12@gmail.com>
2025-12-05 16:27:30 +0000

    Refactor: Improve code formatting and streamline middleware setup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { DB } from './database/db.js';
+import {DB} from './database/db.js';
 import { PORT } from './config/env.js';
 import authrouter from './Routes/auth.route.js';
 import patientRouter from './Routes/patient.route.js';
@@ -7,15 +7,17 @@ import prescriptionRouter from './Routes/patient.route.js';
 import walletRouter from './Routes/wallet.route.js';
 import paystackRouter from './Routes/paystack.route.js';
 import bodyParser from 'body-parser';
+
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+
 const app = express();
 
-// Middlewares
+//Middlewares
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use (bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
 app.use(cors({
     origin: ['http://localhost:5000'],
     credentials: true,
@@ -24,23 +26,22 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// Routes
+//Routes
 app.use('/api/v1/auth', authrouter);
 app.use('/api/v1/patients', patientRouter);
 app.use('/api/v1/prescriptions', prescriptionRouter);
 app.use('/api/v1/wallets', walletRouter);
-app.use('/api/v1/hms/pay', paystackRouter);
+app.use('/api/v1/hms/pay', paystackRouter)
 
-const startServer = async () => {
-    try {
-        await DB(); // Connect to MongoDB FIRST
-        app.listen(PORT, () => {
-            console.log(`Server is running`);
-        });
-    } catch (error) {
-        console.error("Failed to start server:", error);
-        process.exit(1);
-    }
-};
 
-startServer();
+
+
+
+
+
+
+
+app.listen(PORT, async() =>{
+   await DB();
+    console.log('server is running')
+})
