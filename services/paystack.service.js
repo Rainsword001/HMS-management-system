1b6e4025eac16fa35b9946814a26209a13f4459a
Rainsword001 <olowoyoemmanuel12@gmail.com>
2025-12-05 15:26:03 +0000

    Refactor: Update Paystack service to use axios for HTTP requests and improve error handling

--- services/paystack.service.js
+++ services/paystack.service.js
@@ -1,82 +1,77 @@
-
 import request from "request";
 import Payments from "../models/paystack.model.js";
-import paystack from '../utils/paystack.util.js';
+import paystack from "../utils/paystack.util.js";
 import _ from "lodash";
 
-const {initializePayment, verifyPayment}= paystack(request)
+const { initializePayment, verifyPayment } = paystack(request);
 
-class PaymentService{
-    startPayment(data) {
-        return new Promise(async (resolve, reject) => {
-            try{
-                const form = _.pick(data, ['amount', 'email', 'full_name']);
-                form.metadata ={
-                    full_name: form.full_name
-                }
-                form.amount *= 100;
+class PaymentService {
+  constructor() {}
 
+  startPayment(data) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const form = _.pick(data, ["amount", "email", "full_name"]);
+        form.metadata = {
+          full_name: form.full_name,
+        };
+        form.amount *= 100;
 
+        initializePayment(form, (error, body) => {
+          if (error != null) {
+            reject(error.message);
+          }
+          const response = body;
 
-                initializePayment(form, (error, body) => {
-                    if(error){
-                        reject(error.message)
-                    }
-                    const response = JSON.parse(body);
-                
-                    return resolve(response)
-                })
-            } catch(error){
-                error.source = 'Start payment service';
-                return reject(error);
-            }
-        })
+          return resolve(response);
+        });
+      } catch (error) {
+        error.source = "Start payment service";
+        return reject(error);
+      }
+    });
+  }
+
+  createPayment(req) {
+    const ref = req.reference;
+    if (ref == nul) {
+      return reject({ code: 400, msg: "No reference passed in query!" });
     }
+    return new Promise(async (resolve, reject) => {
+      try {
+        verifyPayment(ref, (error, body) => {
+          if (error) {
+            reject(error.message);
+          }
+          const response = JSON.parse(body);
 
+          const { reference, amount, status } = response.data;
+          const { email } = response.data.customer;
+          const full_name = response.data.metadata.full_name;
+          const newPayment = { reference, amount, email, full_name, status };
+          const payment = Payments.create(newPayment);
 
-    createPayment(req){
-        const ref = req.reference;
-        if(ref == nul){
-            return reject({code: 400, msg: 'No reference passed in query!'})
-        }
-        return new Promise(async (resolve, reject) =>{
-            try {
-                verifyPayment(ref, (error, body) =>{
-                    if(error){
-                        reject(error.message)
-                    }
-                    const response = JSON.parse(body);
+          return resolve(payment);
+        });
+      } catch (error) {
+        error.source = "Create payment service";
+        return reject(error);
+      }
+    });
+  }
 
-                    const {reference, amount, status} = response.data;
-                    const {email} = response.data.customer;
-                    const full_name = response.data.metadata.full_name;
-                    const newPayment = {reference, amount, email, full_name, status};
-                    const payment = Payments.create(newPayment)
-
-
-                    return resolve(payment)
-                })
-            } catch (error) {
-                 error.source = 'Create payment service';
-                return reject(error);
-            }
-        })
-    }
-
-    paymentReciept(body){
-        return new Promise(async (resolve, reject) =>{
-            try {
-                const reference = body.reference;
-                const transaction = Payments.findOne({reference: reference})
-                return resolve(transaction)
-            } catch (error) {
-                 error.source = 'Payment Reciept';
-                return reject(error);
-            }
-        })
-    }
+  paymentReciept(body) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const reference = body.reference;
+        const transaction = Payments.findOne({ reference: reference });
+        return resolve(transaction);
+      } catch (error) {
+        error.source = "Payment Reciept";
+        return reject(error);
+      }
+    });
+  }
 }
 
-
-
-export default PaymentService
+export default PaymentService;
--- utils/paystack.util.js
+++ utils/paystack.util.js
@@ -1,46 +1,48 @@
+import axios from "axios";
 
+const paystack = (request) => {
+  const MySecretKey = process.env.PAYSTACK_SECRET_KEY;
 
-const paystack = (request) =>{
-    const MySecretKey = process.env.PAYSTACK_SECRET_KEY
+  //initialize payment
+  const initializePayment = async (form, mycallback) => {
+    try {
+    } catch (error) {}
+    const response = await axios.post(
+      `${process.env.PAYSTACK_BASE_URL}/transaction/initialize`,
+      form,
+      {
+        headers: {
+          authorization: `Bearer ${MySecretKey}`,
+          "Content-type": "application/json",
+          "cache-control": "no-cache",
+        },
+      }
+    );
+    return mycallback(
+      !response.data.status ? "Error Occures" : null,
+      response.data
+    );
+  };
 
-    //initialize payment
-    const initializePayment = (form, mycallback) =>{
-        const options = {
-            url: 'https://api.paystack.co/transaction/initialize',
-            headers: {
-                authorization: MySecretKey,
-                'Content-type': 'application/json',
-                'cache-control' : 'no-cache'
-            },
-            form
-        }
+  //verify payment
+  const verifyPayment = (ref, mycallback) => {
+    const options = {
+      url:
+        "https://api.paystack.co/transaction/verify/" +
+        encodedURIComponent(ref),
+      hearders: {
+        autorization: MySecretKey,
+        "Content-type": "application/json",
+        "cache-control": "no-cache",
+      },
+    };
+    const callback = (error, response, body) => {
+      return mycallback(error, body);
+    };
+    request(options, callback);
+  };
 
-        const callback = (error, response, body) =>{
-            return mycallback(error, body)
-        }
-    }
+  return { initializePayment, verifyPayment };
+};
 
-
-    //verify payment
-    const verifyPayment = (ref, mycallback)=>{
-        const options = {
-            url: 'https://api.paystack.co/transaction/verify/'+encodedURIComponent(ref),
-            hearders:{
-               autorization: MySecretKey,
-               'Content-type': 'application/json',
-                'cache-control' : 'no-cache'
-            }
-        }
-        const callback = (error, response, body) =>{
-            return mycallback(error, body)
-        }
-        request(options, callback)
-    }
-
-    return {initializePayment, verifyPayment}
-
-
-}
-
-
-export default paystack
+export default paystack;
